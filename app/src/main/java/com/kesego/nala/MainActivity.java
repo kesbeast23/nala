c6be5b15e05e0c668c722697782c4c6dbab1ff5d
Kesego Mokgosi <kesegomokgosi23@gmail.com>
2022-01-22 22:29:58 +0200

    grad up

--- app/src/main/java/com/kesego/nala/MainActivity.java
+++ app/src/main/java/com/kesego/nala/MainActivity.java
@@ -1,14 +1,64 @@
 package com.kesego.nala;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.kesego.nala.fragments.HomeFragment;
+import com.kesego.nala.fragments.NotificationFragment;
+import com.kesego.nala.fragments.ProfileFragment;
+import com.kesego.nala.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
+    Fragment selectFragment = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
     }
+
+    private  BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectFragment = new HomeFragment();
+                    break;
+                case R.id.nav_search:
+                    selectFragment = new SearchFragment();
+                    break;
+                case R.id.nav_add:
+                    selectFragment=null;
+                    startActivity(new Intent(MainActivity.this,PostActivity.class));
+                    break;
+                case R.id.nav_heart:
+                    selectFragment = new NotificationFragment();
+                    break;
+                case R.id.nav_profile:
+                    SharedPreferences.Editor editor = getSharedPreferences("PREFS",MODE_PRIVATE).edit();
+                    editor.putString("profileid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    editor.apply();
+                    selectFragment = new ProfileFragment();
+                    break;
+            }
+            if (selectFragment != null){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectFragment).commit();
+                return true;
+            }
+            return false;
+        }
+    };
 }
---
+++ app/src/main/java/com/kesego/nala/PostActivity.java
@@ -0,0 +1,25 @@
+package com.kesego.nala;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.kesego.nala.fragments.HomeFragment;
+import com.kesego.nala.fragments.SearchFragment;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+
+
+    }
+}
