f0ff210118de6f5b769943366836098188437dbc
Kesego Mokgosi <kesegomokgosi23@gmail.com>
2022-01-24 01:37:04 +0200

    grad up

---
+++ app/src/main/java/com/kesego/nala/Adapter/PostAdapter.java
@@ -0,0 +1,143 @@
+package com.kesego.nala.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.kesego.nala.Model.Post;
+import com.kesego.nala.Model.User;
+import com.kesego.nala.R;
+
+import java.util.List;
+
+public class PostAdapter extends  RecyclerView.Adapter<PostAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<Post> mPost;
+
+    FirebaseUser firebaseUser;
+
+    public PostAdapter(Context mContext, List<Post> mPost) {
+        this.mContext = mContext;
+        this.mPost = mPost;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.post_item,parent,false);
+        return new PostAdapter.ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        Post post = mPost.get(position);
+
+        Glide.with(mContext).load(post.getPostimage()).into(holder.post_image);
+        if (post.getDescription().equals("")){
+            holder.description.setVisibility(View.GONE);
+        }else{
+            holder.description.setVisibility(View.VISIBLE);
+            holder.description.setText(post.getDescription());
+        }
+
+        publisherInfo(holder.image_profile,holder.username,holder.publisher,post.getPublisher());
+        isLiked(post.getPostid(),holder.like);
+
+        holder.like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.like.getTag().equals("like")){
+                    FirebaseDatabase.getInstance().getReference().child("Likes").child(post.getPostid()).child(firebaseUser.getUid()).setValue(true);
+                }else{
+                    FirebaseDatabase.getInstance().getReference().child("Likes").child(post.getPostid()).child(firebaseUser.getUid()).removeValue();
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPost.size();
+    }
+
+    public  class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView image_profile,post_image,like,share,delete;
+        public TextView username,likes,description,publisher;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image_profile = itemView.findViewById(R.id.image_profile);
+            post_image = itemView.findViewById(R.id.post_image);
+            like = itemView.findViewById(R.id.like);
+            share = itemView.findViewById(R.id.share);
+            delete = itemView.findViewById(R.id.delete);
+            username = itemView.findViewById(R.id.username);
+            likes = itemView.findViewById(R.id.likes);
+            description = itemView.findViewById(R.id.description);
+            publisher = itemView.findViewById(R.id.publisher1);
+        }
+    }
+    private  void isLiked(String postid,ImageView imageView){
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Likes").child(postid);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.child(firebaseUser.getUid()).exists()){
+                    imageView.setImageResource(R.drawable.ic_liked);
+                    imageView.setTag("liked");
+                }else{
+                    imageView.setImageResource(R.drawable.ic_like1);
+                    imageView.setTag("like");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void  publisherInfo(final ImageView image_profile,final TextView username,final TextView publisher,String userid){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                Glide.with(mContext).load(user.getImageurl()).into(image_profile);
+                username.setText(user.getUsername());
+                publisher.setText(user.getUsername());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/kesego/nala/fragments/HomeFragment.java
+++ app/src/main/java/com/kesego/nala/fragments/HomeFragment.java
@@ -2,26 +2,79 @@ package com.kesego.nala.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.kesego.nala.Adapter.PostAdapter;
+import com.kesego.nala.Model.Post;
+import com.kesego.nala.Model.User;
 import com.kesego.nala.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> postList;
+    private String userid;
 
-
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        postList = new ArrayList<>();
+        postAdapter = new PostAdapter(getContext(),postList);
+        recyclerView.setAdapter(postAdapter);
+        readPosts();
+
+        return  view;
+
+    }
+    public void  readPosts(){
+        userid =  FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList.clear();
+                for (DataSnapshot snapshot1:snapshot.getChildren()){
+                    Post post = snapshot1.getValue(Post.class);
+                    if(post.getPublisher().equals(userid)){
+                        postList.add(post);
+                    }
+                }
+                postAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
